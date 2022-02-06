18db90c5c6411219198eb9170388303234137c94
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-02-06 22:53:53 +0100

    añadida condición para cuando gane cualquiera de los 8 jugadores con su texto correspondiente

--- js/characters.js
+++ js/characters.js
@@ -17,15 +17,21 @@ class Character {
 // ---------------------------------------------------------------- INSTANCIAS ---------------------------------------------------------
 //Declaro las instancias de los personajes
 let challenger1 = new Character("Zorra", 7000, 350, 50, 10);
-let challenger2 = new Character("Siete", 4650, 350, 25, 10);
+let challenger2 = new Character("Siete", 4650, 3350, 25, 10);
+let challenger3 = new Character("S", 4650, 3350, 25, 10);
+let challenger4 = new Character("A", 4650, 3350, 25, 10);
+let challenger5 = new Character("E", 4650, 3350, 25, 10);
+let challenger6 = new Character("F", 4650, 3350, 25, 10);
+let challenger7 = new Character("I", 4650, 3350, 25, 10);
+let challenger8 = new Character("L", 4650, 3350, 25, 10);
 
 const dañarVida = () => {
   
  
   challenger1.health = parseInt(challenger1.health)  - (parseInt(challenger2.attack * Math.random(challenger2.lucky) ));
   challenger2.health = parseInt(challenger2.health)  - (parseInt(challenger1.attack * Math.random(challenger1.lucky) ));
-  console.log(challenger1.health);
-  console.log(challenger2.health);
+
+  
   //Condición que dictamina el ganador/perdedor
   if (challenger1.health <= 0) {
     challenger1.status = 'LOSER'
@@ -36,16 +42,45 @@ const dañarVida = () => {
     challenger1.status = 'WINNER'
   }
   //Condición que hace cambiar de la vista 3 a la 4 cuando acaba el combate
-  if (challenger1.status == 'WINNER' || challenger2.status == 'WINNER') {
+let winner = document.getElementById('infoWin')  
+  if (challenger1.status == 'WINNER') {
     changeView(4);
-  }
+winner.innerHTML = `Ha ganado challenger1` 
+  }else if (challenger2.status == 'WINNER'){
+    changeView(4);
+    winner.innerHTML = `Ha ganado challenger2` 
+  }else if (challenger3.status == 'WINNER'){
+    changeView(4);
+    winner.innerHTML = `Ha ganado challenger3` 
+  }else if (challenger4.status == 'WINNER'){
+    changeView(4);
+    winner.innerHTML = `Ha ganado challenger4` 
+  }else if (challenger5.status == 'WINNER'){
+    changeView(4);
+    winner.innerHTML = `Ha ganado challenger5` 
+  }else if (challenger6.status == 'WINNER'){
+    changeView(4);
+    winner.innerHTML = `Ha ganado challenger6` 
+  }else if (challenger7.status == 'WINNER'){
+    changeView(4);
+    winner.innerHTML = `Ha ganado challenger7` 
+  }else if (challenger8.status == 'WINNER'){
+  changeView(4);
+  winner.innerHTML = `Ha ganado challenger8` 
 };
-
+}
 
 //Guardo los personajes en un diccionario
 let SaveCharacters = {
   1: challenger1,
   2: challenger2,
+  3: challenger3,
+  4: challenger4,
+  5: challenger5,
+  6: challenger6,
+  7: challenger7,
+  8: challenger8,
+  
 };
 
 
