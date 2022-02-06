f1831bb97e9919abc3425a63e6574f37ddd86861
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-02-06 16:59:55 +0100

    añadida funcionalidad de combate

--- js/characters.js
+++ js/characters.js
@@ -1,41 +1,40 @@
 //                                 JS PARA PONER TODO LO QUE TIENE VER CON LAS CLASES DE PERSONAJE Y SUS ACCIONES
 
 //-------------------------------------------- APARTADO PARA LA CLASE Y SUS INSTANCIAS ---------------------------------------------------------
-const btnBattle = document.getElementById('battle')
+const btnBattle = document.getElementById("battle");
 class Character {
-  constructor(nick, health, attack, speed) {
-    this.lucky = Math.floor(Math.random() * 13);
+  constructor(nick, health, attack, speed, lucky) {
+    this.lucky = lucky;
     this.nick = nick;
     this.health = health;
-    this.attack = attack;
-    this.speed = speed;
+    this.attack = parseInt(attack);
+    this.speed = parseInt(speed);
   }
-  dañarVida(health,attack) {
-    console.log("hola",this.health);
-    this.health = health - attack;
-  }
+
 }
 
-// ---------------------------------------------------------------- INSTANCIAS ---------------------------------------------------------console.log(health)
+// ---------------------------------------------------------------- INSTANCIAS ---------------------------------------------------------
 //Declaro las instancias de los personajes
-let challenger1 = new Character("Zorra", 7000, 350, 50);
-let challenger2 = new Character("Siete", 4650, 350, 25);
+let challenger1 = new Character("Zorra", 7000, 350, 50, 10);
+let challenger2 = new Character("Siete", 4650, 350, 25, 10);
 
 const dañarVida = () => {
-  challenger1.health - challenger2.attack * challenger2.lucky;
-  challenger2.health - challenger1.attack * challenger1.attack;
+  console.log('funcion externa');
+ 
+  challenger1.health = parseInt(challenger1.health)  - (parseInt(challenger2.attack * Math.random(challenger2.lucky) ));
+  challenger2.health = parseInt(challenger2.health)  - (parseInt(challenger1.attack * Math.random(challenger1.lucky) ));
   console.log(challenger1.health);
   console.log(challenger2.health);
 };
 console.log(challenger1);
-
+console.log(challenger2.lucky + 'Soy la suerte');
 
 //Guardo los personajes en un diccionario
 let SaveCharacters = {
   1: challenger1,
-  2: challenger2
-}
+  2: challenger2,
+};
 
 console.log(SaveCharacters);
 console.log(challenger1.health);
-btnBattle.addEventListener('click',challenger1.dañarVida())
+btnBattle.addEventListener("onclick",dañarVida());
