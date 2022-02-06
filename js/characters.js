bc5b18780cba98e20cb86755db506f891ad75676
AidaRP <90700748+AidaRP@users.noreply.github.com>
2022-02-06 13:18:16 +0100

    solucionado problema de la función selección de personaje

--- js/characters.js
+++ js/characters.js
@@ -1,43 +1,41 @@
-
-//                                 JS PARA PONER TODO L OQUE TIENE VER CON LAS CLASES DE PERSONAJE Y SUS ACCIONES
-
-
-
+//                                 JS PARA PONER TODO LO QUE TIENE VER CON LAS CLASES DE PERSONAJE Y SUS ACCIONES
 
 //-------------------------------------------- APARTADO PARA LA CLASE Y SUS INSTANCIAS ---------------------------------------------------------
-
-class Character{
-    constructor(nick, health, attack, speed){
-        this.nick = nick,
-        this.health = health,
-        this.attack = attack,
-        this.lucky = math.floor(Math.random() * 13)
-        this.speed = speed
-    }
-    
-       
+const btnBattle = document.getElementById('battle')
+class Character {
+  constructor(nick, health, attack, speed) {
+    this.lucky = Math.floor(Math.random() * 13);
+    this.nick = nick;
+    this.health = health;
+    this.attack = attack;
+    this.speed = speed;
+  }
+  dañarVida(health,attack) {
+    console.log("hola",this.health);
+    this.health = health - attack;
+  }
 }
 
-// class Player {
-//     constructor(player){
-//         this.player = player
-//     }
-//     golpe(playerLostLife, playerDoDmg){
-//         let = lifeValue;
-//         lifeValue = playerLostLife.player.health - playerDoDmg.player.attack
-//         return playerLostLife.player.health = let
-//     }
-
-// }
-// ---------------------------------------------------------------- INSTANCIAS ---------------------------------------------------------
-
+// ---------------------------------------------------------------- INSTANCIAS ---------------------------------------------------------console.log(health)
 //Declaro las instancias de los personajes
-let player1 = new Character('Aída', 1000, 350);
-let player2 = new Character('Siete', 1000, 350);
+let challenger1 = new Character("Zorra", 7000, 350, 50);
+let challenger2 = new Character("Siete", 4650, 350, 25);
+
+const dañarVida = () => {
+  challenger1.health - challenger2.attack * challenger2.lucky;
+  challenger2.health - challenger1.attack * challenger1.attack;
+  console.log(challenger1.health);
+  console.log(challenger2.health);
+};
+console.log(challenger1);
 
 
 //Guardo los personajes en un diccionario
 let SaveCharacters = {
-    1: player1,
-    2: player2,
+  1: challenger1,
+  2: challenger2
 }
+
+console.log(SaveCharacters);
+console.log(challenger1.health);
+btnBattle.addEventListener('click',challenger1.dañarVida())
--- js/main.js
+++ js/main.js
@@ -1,32 +1,50 @@
-
-//-------------------------------------------------------------------- APARTADO DE FUNCIONES ------------------------------------------------------------------------------- 
-
+//-------------------------------------------------------------------- APARTADO DE FUNCIONES -------------------------------------------------------------------------------
 // Función de cambio de Vista
 const changeView = (vw_0) => {
+  let fasewant = "view" + vw_0;
 
-    let fasewant = "view" + vw_0;
+  let arrFases = ["view1", "view2", "view3", "view4"];
 
-    
-    let arrFases = ["view1", "view2", "view3", "view4"];
+  arrFases = arrFases.filter((val) => !fasewant.includes(val));
 
-    arrFases = arrFases.filter(val => !fasewant.includes(val));
+  document.getElementById(fasewant).style.display = "block";
 
-    document.getElementById(fasewant).style.display = "block";
-
-    for (let _f of arrFases) {
-        document.getElementById(_f).style.display = "none";
-    }
-
-    
+  for (let _f of arrFases) {
+    document.getElementById(_f).style.display = "none";
+  }
 };
 
-// Función para seleccionar personaje
-const selectionCharacter = (Character) => {
-    (console.log('Acabas de seleccionar a ' + Character));
-}
+// Arrays para guardar los personajes y que pasen de la vista 2 a la 3.
+let player1 = [];
+let player2 = [];
 
+// Función para seleccionar personaje VISTA 2
+const selectionCharacter = (Personaje) => {
+  console.log("Acabas de seleccionar a " + Personaje);
+  console.log(player1);
+  if (player1.length == 0) {
+    player1.push(SaveCharacters[Personaje]);
+
+    console.log(player1);
+  } else if (player2.length == 0) {
+    console.log(player2);
+    player2.push(SaveCharacters[Personaje]);
+    console.log(player2);
+    console.log("mio", player1.length > 0);
+    if (player1 != [] && player2 !== []) {
+      setTimeout(() => {
+        changeView(3);
+      }, 1000);
+    }
+  }
+};
+let round = 0;
+
+const startBattle = (boton) => {
+  if (btnSeleccion == true)
+    if (player1.health == 0 && player2.health == 0) {
+    } else {
+      console.log("No ha entrado");
+    }
+};
 // .innerText() esto se utiliza si queremos renderizar el texto en la vista correspondiente.
-
-                                                                            // COMBATE
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
